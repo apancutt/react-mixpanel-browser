7d061b7d13662225b12927c39f9227f390ffff3f
Adam Pancutt <adam@pancutt.com>
2023-07-15 20:27:52 +0200

    Improve linting/formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,34 +2,30 @@
 module.exports = {
   env: {
     es2023: true,
-    'shared-node-browser': true,
+    node: true,
   },
   extends: [
     'eslint:recommended',
+    'plugin:compat/recommended',
     'plugin:import/recommended',
+    'plugin:prettier/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'prettier',
   ],
   overrides: [
     {
-      env: {
-        node: true,
-      },
-      files: ['bin/**', '.eslintrc.cjs'],
-    },
-    {
-      env: {
-        browser: true,
-      },
-      extends: ['plugin:compat/recommended'],
-      files: ['src/*'],
-    },
-    {
-      extends: ['plugin:@typescript-eslint/strict', 'plugin:import/typescript'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/typescript',
+        'plugin:typescript-sort-keys/recommended',
+      ],
       files: ['**/*.{ts,tsx}'],
       parser: '@typescript-eslint/parser',
-      plugins: ['@typescript-eslint'],
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+      plugins: ['@typescript-eslint', 'typescript-sort-keys'],
       rules: {
         '@typescript-eslint/consistent-type-imports': [
           'error',
@@ -37,24 +33,97 @@ module.exports = {
             fixStyle: 'inline-type-imports',
           },
         ],
+        '@typescript-eslint/dot-notation': 'error',
+        '@typescript-eslint/explicit-member-accessibility': 'error',
+        '@typescript-eslint/member-ordering': [
+          'error',
+          {
+            default: {
+              memberTypes: [
+                'signature',
+                'public-static-field',
+                'protected-static-field',
+                'private-static-field',
+                'public-field',
+                'protected-field',
+                'private-field',
+                'public-constructor',
+                'protected-constructor',
+                'private-constructor',
+                'public-static-method',
+                'protected-static-method',
+                'private-static-method',
+                ['public-get', 'public-set'],
+                ['protected-get', 'protected-set'],
+                ['private-get', 'private-set'],
+                'public-method',
+                'protected-method',
+                'private-method',
+              ],
+              // TODO: Use "natural" once getter/setter collision issue is resolved
+              // See: https://github.com/typescript-eslint/typescript-eslint/issues/6812
+              order: 'alphabetically',
+            },
+          },
+        ],
+        'dot-notation': 'off',
       },
       settings: {
         'import/resolver': {
+          node: true,
           typescript: true,
         },
       },
+    },
+    {
+      env: {
+        browser: true,
+        node: false,
+      },
+      files: ['src/**'],
+      // TODO: Uncomment once https://github.com/amilajack/eslint-plugin-compat/pull/585 is merged
+      // `query` should be "production" browserslist env
+      // rules: {
+      //   'compat/compat': [
+      //     'error',
+      //     {
+      //       query,
+      //       ignoreBrowserslistTargets: true,
+      //     },
+      //   ],
+      // },
     },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
-  plugins: ['react'],
+  plugins: ['prettier', 'sort-keys-shorthand'],
   root: true,
   rules: {
+    'compat/compat': 'off',
+    // TODO: Uncomment once https://github.com/amilajack/eslint-plugin-compat/pull/585 is merged
+    // `query` should be "node" browserslist env
+    // rules: {
+    //   'compat/compat': [
+    //     'error',
+    //     {
+    //       query,
+    //       ignoreBrowserslistTargets: true,
+    //     },
+    //   ],
+    // },
     'default-case': 'error',
     'dot-notation': 'error',
     'grouped-accessor-pairs': ['error', 'getBeforeSet'],
+    'import/consistent-type-specifier-style': ['error', 'prefer-inline'],
+    'import/namespace': [
+      'error',
+      {
+        allowComputed: true,
+      },
+    ],
     'import/no-default-export': 'error',
+    'import/no-duplicates': ['error', { 'prefer-inline': true }],
     'import/no-unresolved': 'off',
     'import/order': [
       'error',
@@ -65,19 +134,21 @@ module.exports = {
         'newlines-between': 'never',
       },
     ],
-    'import/namespace': [
+    'no-constructor-return': 'error',
+    'no-duplicate-imports': ['error', { includeExports: true }],
+    'no-unused-expressions': 'error',
+    'sort-imports': [
       'error',
       {
-        allowComputed: true,
+        ignoreDeclarationSort: true,
       },
     ],
-    'no-constructor-return': 'error',
-    'no-duplicate-imports': 'error',
-    'no-unused-expressions': 'error',
-  },
-  settings: {
-    'import/resolver': {
-      node: true,
-    },
+    'sort-keys-shorthand/sort-keys-shorthand': [
+      'error',
+      'asc',
+      {
+        shorthand: 'first',
+      },
+    ],
   },
 };
